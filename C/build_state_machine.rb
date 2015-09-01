dd2451107e499267094d095c78975bddc2564e45
Lavir the Whiolet <Lavir.th.Whiolet@gmail.com>
2015-09-01 22:13:35 +0500

    ...

--- C/build_state_machine.rb
+++ C/build_state_machine.rb
@@ -1,10 +1,8 @@
-require 'aho_corasick'
+require 'ahocorasick'
 require 'yaml'
 
 words = %W{ ab bd }
 
-ah = AhoCorasick.new(*words)
+ah = AhoC::Trie.new :DFA
+words.each { |word| ah.add word }
 puts ah.to_yaml
-
-
-# @root=#<TreeNode:0x93ea89c @parent=nil, @suffix=nil, @matches=[], @children={:a=>#<TreeNode:0x93ea7fc @parent=#<TreeNode:0x93ea89c ...>, @suffix=#<TreeNode:0x93ea89c ...>, @matches=[], @children={:b=>#<TreeNode:0x93ea7ac @parent=#<TreeNode:0x93ea7fc ...>, @suffix=#<TreeNode:0x93ea70c @parent=#<TreeNode:0x93ea89c ...>, @suffix=#<TreeNode:0x93ea89c ...>, @matches=[], @children={:d=>#<TreeNode:0x93ea6bc @parent=#<TreeNode:0x93ea70c ...>, @suffix=nil, @matches=["bd"], @children={}>}>, @matches=["ab"], @children={}>}>, :b=>#<TreeNode:0x93ea70c @parent=#<TreeNode:0x93ea89c ...>, @suffix=#<TreeNode:0x93ea89c ...>, @matches=[], @children={:d=>#<TreeNode:0x93ea6bc @parent=#<TreeNode:0x93ea70c ...>, @suffix=nil, @matches=["bd"], @children={}>}>}>>
