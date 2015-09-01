50721123a0acc1fd0a02912db5c317b0510efe07
Lavir the Whiolet <Lavir.th.Whiolet@gmail.com>
2015-09-01 21:02:25 +0500

    ...

--- C/build_state_machine.rb
+++ C/build_state_machine.rb
@@ -2,8 +2,20 @@ machine = {}
 
 words = %W{ abc abd }
 
+$next_state = 0
+def new_state
+  $next_state.tap { $next_state += 1 }
+end
+
+INITIAL_STATE = new_state
+
 for word in words
+  state = INITIAL_STATE
   for c, i in word.chars.zip((0...s.size).to_a)
-    
+    if not machine.has_key? [state, c] then
+      machine[[state, c]] = new_state
+    end
+    state = machine[[state, c]]
   end
+  machine[[state, ""]] = INITIAL_STATE
 end
