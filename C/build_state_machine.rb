1f152bf9c1d645bdb32bcbcc18a22c2c401ae750
Lavir the Whiolet <Lavir.th.Whiolet@gmail.com>
2015-09-01 21:05:48 +0500

    ...

--- C/build_state_machine.rb
+++ C/build_state_machine.rb
@@ -7,16 +7,19 @@ def new_state
   $next_state.tap { $next_state += 1 }
 end
 
+class Action < Struct.new(:new_state, :action)
+end
+
 INITIAL_STATE = new_state
 
 for word in words
   state = INITIAL_STATE
   for c, i in word.chars.zip((0...word.size).to_a)
     if not machine.has_key? [state, c] then
-      machine[[state, c]] = new_state
+      machine[[state, c]] = Action.new(new_state, :move)
     end
     if i == (word.size - 1) then
-      machine[[state, c]] = INITIAL_STATE
+      machine[[state, c]] = Action.new(INITIAL_STATE, :return)
     else
       state = machine[[state, c]]
     end
